d0a51b769b28a765f43598fd90ca2aec111850d1
derya <derya@boonandmoil.dev>
2022-08-08 08:43:33 +0100

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Derya Antonelli on 08/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
